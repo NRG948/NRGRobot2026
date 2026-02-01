4c224caa7c9e5cfe986a03f27882b5c64fbb79b7
NRG948DriverStation <programming@nrg948.com>
2026-02-01 14:50:25 -0800

    Updates Shooter Subsystem for new hooded single-roller shooter design.
    Co-Authored-By: Jerry <jerzhang414@gmail.com>

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,11 +7,11 @@
  
 package frc.robot.subsystems;
 
+import static frc.robot.Constants.RobotConstants.CANID.SHOOTER_LOWER_LEFT_ID;
 import static frc.robot.Constants.RobotConstants.CANID.SHOOTER_LOWER_RIGHT_ID;
 import static frc.robot.Constants.RobotConstants.CANID.SHOOTER_UPPER_RIGHT_ID;
 import static frc.robot.Constants.RobotConstants.MAX_BATTERY_VOLTAGE;
 import static frc.robot.util.MotorDirection.CLOCKWISE_POSITIVE;
-import static frc.robot.util.MotorDirection.COUNTER_CLOCKWISE_POSITIVE;
 import static frc.robot.util.MotorIdleMode.COAST;
 
 import com.nrg948.dashboard.annotations.DashboardCommand;
@@ -40,83 +40,43 @@ public class Shooter extends SubsystemBase implements ActiveSubsystem {
 
   private static final DataLog LOG = DataLogManager.getLog();
 
-  private static final MotorParameters LOWER_MOTOR = MotorParameters.KrakenX44;
-  private static final double LOWER_GEAR_RATIO = 1.0;
-  private static final double LOWER_WHEEL_DIAMETER = Units.inchesToMeters(4);
-  private static final double LOWER_METERS_PER_REV =
-      (LOWER_WHEEL_DIAMETER * Math.PI) / LOWER_GEAR_RATIO;
+  private static final MotorParameters SHOOTER_MOTOR = MotorParameters.KrakenX44;
+  private static final double GEAR_RATIO = 1.0;
+  private static final double WHEEL_DIAMETER = Units.inchesToMeters(4);
+  private static final double METERS_PER_REV = (WHEEL_DIAMETER * Math.PI) / GEAR_RATIO;
 
-  private static final MotorParameters UPPER_MOTOR = MotorParameters.KrakenX60;
-  private static final double UPPER_GEAR_RATIO = 3.0;
-  private static final double UPPER_WHEEL_DIAMETER = Units.inchesToMeters(3);
-  private static final double UPPER_METERS_PER_REV =
-      (UPPER_WHEEL_DIAMETER * Math.PI) / UPPER_GEAR_RATIO;
-
-  private final MotorController lowerMotor =
-      LOWER_MOTOR.newController(
-          "/Shooter/Lower Motor",
-          CANID.SHOOTER_LOWER_LEFT_ID,
+  private final MotorController leftUpperMotor =
+      SHOOTER_MOTOR.newController(
+          "/Shooter/Left Upper Motor",
+          CANID.SHOOTER_UPPER_LEFT_ID,
           CLOCKWISE_POSITIVE,
           COAST,
-          LOWER_METERS_PER_REV);
-  private final MotorController lowerMotor2 =
-      lowerMotor.createFollower(SHOOTER_LOWER_RIGHT_ID, true);
-  private final MotorController upperMotor =
-      UPPER_MOTOR.newController(
-          "/Shooter/Upper Motor",
-          CANID.SHOOTER_UPPER_LEFT_ID,
-          COUNTER_CLOCKWISE_POSITIVE,
-          COAST,
-          UPPER_METERS_PER_REV);
-  private final MotorController upperMotor2 =
-      upperMotor.createFollower(SHOOTER_UPPER_RIGHT_ID, true);
+          METERS_PER_REV);
 
-  @DashboardTextDisplay(
-      title = "Lower Max Velocity (m/s)",
-      column = 0,
-      row = 3,
-      width = 2,
-      height = 1)
-  private static final double LOWER_MAX_VELOCITY =
-      LOWER_MOTOR.getFreeSpeedRPM() * LOWER_METERS_PER_REV / 60.0;
+  private final MotorController rightUpperMotor =
+      leftUpperMotor.createFollower(SHOOTER_UPPER_RIGHT_ID, true);
+  private final MotorController rightLowerMotor =
+      leftUpperMotor.createFollower(SHOOTER_LOWER_RIGHT_ID, true);
+  private final MotorController leftLowerMotor =
+      leftUpperMotor.createFollower(SHOOTER_LOWER_LEFT_ID, false);
 
-  @DashboardTextDisplay(
-      title = "Upper Max Velocity (m/s)",
-      column = 2,
-      row = 3,
-      width = 2,
-      height = 1)
-  private static final double UPPER_MAX_VELOCITY =
-      UPPER_MOTOR.getFreeSpeedRPM() * UPPER_METERS_PER_REV / 60.0;
+  @DashboardTextDisplay(title = "Max Velocity (m/s)", column = 0, row = 3, width = 2, height = 1)
+  private static final double MAX_VELOCITY =
+      SHOOTER_MOTOR.getFreeSpeedRPM() * METERS_PER_REV / 60.0;
 
-  private static final double LOWER_KS = LOWER_MOTOR.getKs();
-  private static final double UPPER_KS = UPPER_MOTOR.getKs();
-  private static final double LOWER_KV = (MAX_BATTERY_VOLTAGE - LOWER_KS) / LOWER_MAX_VELOCITY;
-  private static final double UPPER_KV = (MAX_BATTERY_VOLTAGE - UPPER_KS) / UPPER_MAX_VELOCITY;
+  private static final double KS = SHOOTER_MOTOR.getKs();
+  private static final double KV = (MAX_BATTERY_VOLTAGE - KS) / MAX_VELOCITY;
 
-  private final RelativeEncoder lowerEncoder = lowerMotor.getEncoder();
-  private final RelativeEncoder upperEncoder = upperMotor.getEncoder();
+  private final RelativeEncoder encoder = leftUpperMotor.getEncoder();
 
-  private final SimpleMotorFeedforward lowerFeedforward =
-      new SimpleMotorFeedforward(LOWER_KS, LOWER_KV);
-  private final SimpleMotorFeedforward upperFeedforward =
-      new SimpleMotorFeedforward(UPPER_KS, UPPER_KV);
+  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(KS, KV);
 
-  @DashboardPIDController(title = "Lower PID", column = 6, row = 0, width = 2, height = 3)
-  private final PIDControllerPreference lowerPIDController =
-      new PIDControllerPreference("Shooter", "Lower PID Controller", 1, 0, 0);
+  @DashboardPIDController(title = "PID", column = 6, row = 0, width = 2, height = 3)
+  private final PIDControllerPreference PIDController =
+      new PIDControllerPreference("Shooter", "PID Controller", 1, 0, 0);
 
-  @DashboardPIDController(title = "Upper PID", column = 8, row = 0, width = 2, height = 3)
-  private final PIDControllerPreference upperPIDController =
-      new PIDControllerPreference("Shooter", "Upper PID Controller", 1, 0, 0);
-
-  @DashboardTextDisplay(
-      title = "Lower Goal Velocity (m/s)",
-      column = 0,
-      row = 2,
-      width = 2,
-      height = 1)
-  private double lowerGoalVelocity = 0;
+  @DashboardTextDisplay(title = "Goal Velocity (m/s)", column = 0, row = 2, width = 2, height = 1)
+  private double goalVelocity = 0;
 
   @DashboardRadialGauge(
       title = "Lower Velocity (m/s)",
@@ -126,45 +86,17 @@ public class Shooter extends SubsystemBase implements ActiveSubsystem {
       height = 2,
       min = -41.270725699090676,
       max = 41.270725699090676)
-  private double lowerCurrentVelocity = 0; // Different Velocity for upper and lower pairs of motors
+  private double currentVelocity = 0;
 
   @DashboardTextDisplay(
-      title = "Upper Goal Velocity (m/s)",
-      column = 2,
-      row = 2,
-      width = 2,
-      height = 1)
-  private double upperGoalVelocity = 0;
-
-  @DashboardRadialGauge(
-      title = "Upper Velocity (m/s)",
-      column = 2,
-      row = 0,
-      width = 2,
-      height = 2,
-      min = -7.979645340118073,
-      max = 7.979645340118073)
-  private double upperCurrentVelocity = 0;
-
-  @DashboardTextDisplay(
-      title = "Test Lower Velocity (m/s)",
+      title = "Test Velocity (m/s)",
       column = 4,
       row = 0,
       width = 2,
       height = 1,
       dataBinding = DataBinding.READ_WRITE,
       showSubmitButton = true)
-  private double testLowerGoalVelocity = 0;
-
-  @DashboardTextDisplay(
-      title = "Test Upper Velocity (m/s)",
-      column = 4,
-      row = 1,
-      width = 2,
-      height = 1,
-      dataBinding = DataBinding.READ_WRITE,
-      showSubmitButton = true)
-  private double testUpperGoalVelocity = 0;
+  private double testGoalVelocity = 0;
 
   @DashboardCommand(
       title = "Set Test Velocities (m/s)",
@@ -174,7 +106,7 @@ public class Shooter extends SubsystemBase implements ActiveSubsystem {
       height = 1,
       fillWidget = true)
   private Command setTestGoalVelocitiesCommand =
-      Commands.runOnce(() -> setGoalVelocities(testLowerGoalVelocity, testUpperGoalVelocity), this)
+      Commands.runOnce(() -> setGoalVelocity(testGoalVelocity), this)
           .withName("Set Test Velocities");
 
   @DashboardCommand(
@@ -187,76 +119,55 @@ public class Shooter extends SubsystemBase implements ActiveSubsystem {
   private Command disableCommand =
       Commands.runOnce(this::disable, this).ignoringDisable(true).withName("Disable");
 
-  private DoubleLogEntry logLowerGoalVelocity =
-      new DoubleLogEntry(LOG, "/Shooter/Lower Goal Velocity");
-  private DoubleLogEntry logUpperGoalVelocity =
-      new DoubleLogEntry(LOG, "/Shooter/Upper Goal Velocity");
-  private DoubleLogEntry logLowerCurrentVelocity =
-      new DoubleLogEntry(LOG, "/Shooter/Lower Current Velocity");
-  private DoubleLogEntry logUpperCurrentVelocity =
-      new DoubleLogEntry(LOG, "/Shooter/Upper Current Velocity");
+  private DoubleLogEntry logGoalVelocity = new DoubleLogEntry(LOG, "/Shooter/Goal Velocity");
+  private DoubleLogEntry logCurrentVelocity = new DoubleLogEntry(LOG, "/Shooter/Current Velocity");
 
   /** Creates a new Shooter. */
   public Shooter() {}
 
-  public void setGoalVelocities(double lowerGoalVelocity, double upperGoalVelocity) {
-    this.lowerGoalVelocity = lowerGoalVelocity;
-    this.upperGoalVelocity = upperGoalVelocity;
-    logLowerGoalVelocity.append(lowerGoalVelocity);
-    logUpperGoalVelocity.append(upperGoalVelocity);
+  public void setGoalVelocity(double goalVelocity) {
+    this.goalVelocity = goalVelocity;
+    logGoalVelocity.append(goalVelocity);
   }
 
   @Override
   public void disable() {
-    lowerGoalVelocity = 0;
-    upperGoalVelocity = 0;
-    logLowerGoalVelocity.append(0);
-    logUpperGoalVelocity.append(0);
-    lowerMotor.stopMotor();
-    lowerMotor2.stopMotor();
-    upperMotor.stopMotor();
-    upperMotor2.stopMotor();
+    goalVelocity = 0;
+    logGoalVelocity.append(0);
+    leftUpperMotor.stopMotor();
+    leftLowerMotor.stopMotor();
+    rightLowerMotor.stopMotor();
+    rightUpperMotor.stopMotor();
   }
 
   @Override
   public void setIdleMode(MotorIdleMode idleMode) {
-    lowerMotor.setIdleMode(idleMode);
-    lowerMotor2.setIdleMode(idleMode);
-    upperMotor.setIdleMode(idleMode);
-    upperMotor2.setIdleMode(idleMode);
+    leftUpperMotor.setIdleMode(idleMode);
+    leftLowerMotor.setIdleMode(idleMode);
+    rightLowerMotor.setIdleMode(idleMode);
+    rightUpperMotor.setIdleMode(idleMode);
   }
 
   @Override
   public void periodic() {
     updateTelemetry();
-    if (lowerGoalVelocity != 0) {
-      double feedforward = this.lowerFeedforward.calculate(lowerGoalVelocity);
-      double feedback = lowerPIDController.calculate(lowerCurrentVelocity, lowerGoalVelocity);
+    if (goalVelocity != 0) {
+      double feedforward = this.feedforward.calculate(goalVelocity);
+      double feedback = PIDController.calculate(currentVelocity, goalVelocity);
       double motorVoltage = feedforward + feedback;
-      lowerMotor.setVoltage(motorVoltage);
+      leftUpperMotor.setVoltage(motorVoltage);
 
     } else {
-      lowerMotor.setVoltage(0);
-    }
-    if (upperGoalVelocity != 0) {
-      double feedforward = this.upperFeedforward.calculate(upperGoalVelocity);
-      double feedback = upperPIDController.calculate(upperCurrentVelocity, upperGoalVelocity);
-      double motorVoltage = feedforward + feedback;
-      upperMotor.setVoltage(motorVoltage);
-
-    } else {
-      upperMotor.setVoltage(0);
+      leftUpperMotor.setVoltage(0);
     }
   }
 
   private void updateTelemetry() {
-    lowerCurrentVelocity = lowerEncoder.getVelocity();
-    upperCurrentVelocity = upperEncoder.getVelocity();
-    logLowerCurrentVelocity.append(lowerCurrentVelocity);
-    logUpperCurrentVelocity.append(upperCurrentVelocity);
-    lowerMotor.logTelemetry();
-    lowerMotor2.logTelemetry();
-    upperMotor.logTelemetry();
-    upperMotor2.logTelemetry();
+    currentVelocity = encoder.getVelocity();
+    logCurrentVelocity.append(currentVelocity);
+    leftUpperMotor.logTelemetry();
+    leftLowerMotor.logTelemetry();
+    rightLowerMotor.logTelemetry();
+    rightUpperMotor.logTelemetry();
   }
 }
