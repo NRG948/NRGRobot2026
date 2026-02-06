cb48a8ed3de5d883a23b37f23c05c5f3ff7f0147
Pranav Dasan <pranavdasan8@gmail.com>
2026-02-05 20:58:06 -0800

    Updated constants based on intake arm

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,21 +4,17 @@
  * Open Source Software; you can modify and/or share it under the terms of
  * the license file in the root directory of this project.
  */
-
+ 
 package frc.robot;
 
 import edu.wpi.first.math.util.Units;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -41,9 +37,9 @@ public final class Constants {
     }
 
     public static final class CANID {
-      public static final int INTAKE_ID = 9;
-      public static final int INTAKE_ARM_ID = 10;
-      public static final int SHOOTER_LOWER_LEFT_ID = 13;
+      public static final int INTAKE_ID = 13;
+      public static final int INTAKE_ARM_ID = 12;
+      public static final int SHOOTER_LOWER_LEFT_ID = 1;
       public static final int SHOOTER_LOWER_RIGHT_ID = 14;
       public static final int SHOOTER_UPPER_LEFT_ID = 11;
       public static final int SHOOTER_UPPER_RIGHT_ID = 12;
