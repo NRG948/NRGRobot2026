c930fb9581b51ccca594a9cf48e5db62592cb0a2
Jiayue-Quan <jiayue.quan@gmail.com>
2026-02-12 22:25:22 -0800

    Starts Elevator subsystem

--- src/main/java/frc/robot/parameters/ElevatorLevel.java
+++ src/main/java/frc/robot/parameters/ElevatorLevel.java
@@ -2,14 +2,15 @@ package frc.robot.parameters;
 
 public enum ElevatorLevel {
     // Levels: Stowed, Level 1, Level 2, Level 3
-    // TODO: add heights for every level
+    // TODO: add heights for every bar level
     STOWED(0),
-    L1(0),
-    L2(0),
-    L3(0);
+    Bar1(5),
+    Bar2(10),
+    Bar3(20);
 
     // variables
     private final double elevatorHeight;
+    
 
     // constructor 
     private ElevatorLevel (double height) {
@@ -20,4 +21,5 @@ public enum ElevatorLevel {
     public double getElevatorHeight() {
         return elevatorHeight;
     }
+
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -8,10 +8,16 @@
 package frc.robot.subsystems;
 
 import static frc.robot.util.MotorDirection.CLOCKWISE_POSITIVE;
+import static frc.robot.util.MotorDirection.COUNTER_CLOCKWISE_POSITIVE;
+
+import java.util.Map;
+
 import static frc.robot.Constants.RobotConstants.MAX_BATTERY_VOLTAGE;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import com.nrg948.preferences.PIDControllerPreference;
+import com.nrg948.dashboard.annotations.DashboardPIDController;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
@@ -23,35 +29,48 @@ import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotPreferences;
+import frc.robot.RobotContainer.RobotSelector;
+import frc.robot.parameters.ElevatorLevel;
+import frc.robot.parameters.MotorParameters;
+import frc.robot.util.MotorController;
 import frc.robot.util.MotorDirection;
 import frc.robot.util.MotorIdleMode;
 import frc.robot.util.TalonFXAdapter;
 import frc.robot.util.RelativeEncoder;
+import static frc.robot.util.MotorIdleMode.BRAKE;
 
 
 public class Elevator extends SubsystemBase implements ActiveSubsystem {
-
-  private final MotorDirection motorDirection = CLOCKWISE_POSITIVE;
-   
-
    // TODO: put actual values
-    private static final double GEAR_RATIO = PARAMETERS.getGearRatio();
+    private static final double GEAR_RATIO = 1; 
   private static final double SPROCKET_DIAMETER = 0.05; // meters
-  private static final double MASS = PARAMETERS.getMass(); // kilograms
+  private static final double MASS = 100; // kilograms
   private static final double POSITION_ERROR_MARGIN = 0.05; 
   private static final double METERS_PER_REVOLUTION = (SPROCKET_DIAMETER * Math.PI) / GEAR_RATIO;
 
-  // TODO: Change placeholder values
-  private final TalonFXAdapter mainMotor =
-      new TalonFXAdapter(
-          "/Elevator", new TalonFX(1, "rio"), motorDirection, MotorIdleMode.BRAKE, 0);
+ private static final MotorParameters MOTOR =
+      RobotPreferences.ROBOT_TYPE.selectOrDefault(
+          Map.of(
+              RobotSelector.AlphaRobot2026, MotorParameters.NullMotor,
+              RobotSelector.CompetitionRobot2026, MotorParameters.KrakenX60,
+              RobotSelector.PracticeRobot2026, MotorParameters.KrakenX60),
+          MotorParameters.NullMotor);
   
-  private final RelativeEncoder encoder = mainMotor.getEncoder();
+  //TODO: Change CAN ID
+  private final MotorController motor =
+      MOTOR.newController(
+          "/Intake/Motor", 1, COUNTER_CLOCKWISE_POSITIVE, BRAKE, METERS_PER_REVOLUTION);
+  
+  private final RelativeEncoder encoder = motor.getEncoder();
+ 
 
-  private boolean isSeekingGoal;
-  private boolean hasError;
-  private final ExponentialProfile.State currentState = new ExponentialProfile.State();
-  private final ExponentialProfile.State goalState = new ExponentialProfile.State();
+  //TODO: Change PID and dashboard value     
+  @DashboardPIDController(title = "PID", column = 7, row = 0, width = 2, height = 3)
+  private final PIDControllerPreference pidController =
+      new PIDControllerPreference("Elevator", "PID Controller", 1, 0, 0);
+
+  // Trapezoid profile values
   private static final DCMotor MOTOR_PARAMS = DCMotor.getKrakenX60(1);
   private final Mechanism2d mechanism2d = new Mechanism2d(0.5, 1.0);
   private final MechanismRoot2d mechanismRoot2d = mechanism2d.getRoot("Elevator Root", 0, 0);
@@ -60,19 +79,11 @@ public class Elevator extends SubsystemBase implements ActiveSubsystem {
       new ElevatorSim(MOTOR_PARAMS, GEAR_RATIO, MASS, SPROCKET_DIAMETER / 2, 0, 1, true, 0);
   private boolean atUpperLimit;
   private boolean atLowerLimit;
-  private static final double MAX_HEIGHT = PARAMETERS.getMaxHeight(); // meters
-  private static final double MIN_HEIGHT = PARAMETERS.getMinHeight(); // meters
+
+  //TODO: Change values
+  private static final double MAX_HEIGHT = 20; // meters
+  private static final double MIN_HEIGHT = 1; // meters
   private static final double DISABLE_HEIGHT = MIN_HEIGHT + 0.01;
-  private final MechanismLigament2d elevatorMech2d =
-      mechanismRoot2d.append(new MechanismLigament2d("Elevator", 0, 90));
-  private static final double POSITION_ERROR_TIME = 2.0; // seconds
-
-
-  private final Timer stuckTimer = new Timer();
-
-  // Trapezoid profile values
-
-  //TODO: replace default values
   private static final double MAX_SPEED =
       (MOTOR_PARAMS.freeSpeedRadPerSec / (2 * Math.PI)) * METERS_PER_REVOLUTION; // m/s
   private static final double MAX_ACCELERATION =
@@ -81,13 +92,28 @@ public class Elevator extends SubsystemBase implements ActiveSubsystem {
       ExponentialProfile.Constraints.fromCharacteristics(
           MAX_BATTERY_VOLTAGE, MAX_SPEED / 2, MAX_ACCELERATION / 64);
 
+  private final MechanismLigament2d elevatorMech2d =
+      mechanismRoot2d.append(new MechanismLigament2d("Elevator", 0, 90));
+  private static final double POSITION_ERROR_TIME = 2.0; // seconds
+
+  private final Timer stuckTimer = new Timer();
+  
   // TODO: Change Feedforward constants
   private static final double KS = 0;
   private static final double KV = 0;
   private static final double KA = 0;
   private static final double KG = 9.81 * KA;
-
   private final ElevatorFeedforward feedForward = new ElevatorFeedforward(KS, KG, KV, KA);
+
+
+  private boolean isSeekingGoal;
+  private boolean hasError;
+
+  private final ExponentialProfile.State currentState = new ExponentialProfile.State();
+  private final ExponentialProfile.State goalState = new ExponentialProfile.State();
+  private ExponentialProfile.State lastState = currentState;
+
+  private ElevatorLevel currentElevatorLevel = ElevatorLevel.STOWED;
 
   /** Creates a new Elevator. */
   public Elevator() {}
@@ -101,6 +127,30 @@ public class Elevator extends SubsystemBase implements ActiveSubsystem {
   public void setIdleMode(MotorIdleMode idleMode) {
     // Do not ever put the elevator in COAST mode or else it will crash down.
   }
+
+  public void setGoalHeight(ElevatorLevel level) {
+    setGoalHeight(level.getElevatorHeight());
+  } 
+
+  /**
+   * Sets elevator goal height in meters.
+   *
+   * @param height the desired height of the elevator in meters.
+   */
+  private void setGoalHeight(double height) {
+    isSeekingGoal = true;
+    goalState.position = height;
+    goalState.velocity = 0;
+    
+    lastState = currentState;
+
+    /*pidController.setPID(KP.getValue(), KI.getValue(), KD.getValue());
+    pidController.reset();*/
+
+  }
+
+
+  
 
   public void disable() {}
 
@@ -133,4 +183,6 @@ private void checkError() {
     }
     hasError = stuckTimer.hasElapsed(POSITION_ERROR_TIME);
   }
+
+
 }
