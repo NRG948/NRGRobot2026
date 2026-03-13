1c50cfde50c5da3cf0188c8f6e39c0935bbafb09
Ed Reed <reed.ej@hotmail.com>
2026-03-12 20:57:37 -0700

    Updates LED count to 61 (#90)
    Per Cyrus, the new LED count on the competition bot is 61.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,12 +28,12 @@ public final class Constants {
   public static class RobotConstants {
     public static final double MAX_BATTERY_VOLTAGE = 12.0;
     public static final double WHEEL_DIAMETER = Units.inchesToMeters(isCompBot() ? 3.957 : 3.75);
-    public static final int LED_COUNT = 32;
+    public static final int LED_COUNT = 61;
     public static final double PERIODIC_INTERVAL = 0.02;
 
     public static class LEDSegment {
       public static final int STATUS_FIRST_LED = 0;
-      public static final int STATUS_LED_COUNT = 32;
+      public static final int STATUS_LED_COUNT = LED_COUNT;
     }
 
     public static class PWMPort {
